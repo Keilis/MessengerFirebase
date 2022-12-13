82db27f1ecc9dc0136f7f1b7e6c04abea9882b5b
Keilis <kurnikovaevgenia@gmail.com>
2022-12-13 16:20:05 +0500

    Creating activities and fix problems

--- app/src/main/java/com/example/messengerfirebase/MainActivity.java
+++ app/src/main/java/com/example/messengerfirebase/MainActivity.java
@@ -28,45 +28,45 @@ public class MainActivity extends AppCompatActivity {
 
         auth.signOut();
 
-        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = auth.getCurrentUser();
-                if (user == null) {
-                    Log.d(LOG_TAG, "No authorized");
-                } else {
-                    Log.d(LOG_TAG, "Authorized " + user.getUid());
-                }
-            }
-        });
-        auth.signInWithEmailAndPassword("email@email.com", "111111")
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
+//        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user = auth.getCurrentUser();
+//                if (user == null) {
+//                    Log.d(LOG_TAG, "No authorized");
+//                } else {
+//                    Log.d(LOG_TAG, "Authorized " + user.getUid());
+//                }
+//            }
+//        });
+//        auth.signInWithEmailAndPassword("email@email.com", "111111")
+//                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                    @Override
+//                    public void onSuccess(AuthResult authResult) {
+//
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d(LOG_TAG, e.getMessage());
+//                        Toast.makeText(MainActivity.this,
+//                                e.getMessage(),
+//                                Toast.LENGTH_SHORT).show();
+//                    }
+//                });
 
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d(LOG_TAG, e.getMessage());
-                        Toast.makeText(MainActivity.this,
-                                e.getMessage(),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-        auth.sendPasswordResetEmail("email@email.com")
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-                    }
-                });
+//        auth.sendPasswordResetEmail("email@email.com")
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void unused) {
+//
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//
+//                    }
+//                });
 
 //        создать нового пользователя
 //        auth.createUserWithEmailAndPassword("email@email.com", "111111")
