1cb3c9ebb53760dec6a34047f52284f3b3fbc013
Keilis <kurnikovaevgenia@gmail.com>
2022-12-14 15:39:18 +0500

    Creating sign out logic

--- app/src/main/java/com/example/messengerfirebase/MessengerActivity.java
+++ app/src/main/java/com/example/messengerfirebase/MessengerActivity.java
@@ -2,6 +2,8 @@ package com.example.messengerfirebase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.content.Intent;
@@ -9,13 +11,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class MessengerActivity extends AppCompatActivity {
-    private LoginActivity mainActivity;
+    private MessengerViewModel messengerViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messenger);
+
+        messengerViewModel = new ViewModelProvider(this).get(MessengerViewModel.class);
+
+        observeViewModel();
+    }
+
+    private void observeViewModel() {
+        messengerViewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser == null) {
+                    launchLoginActivity();
+                    finish();
+                }
+            }
+        });
     }
 
     static Intent newIntent(Context context) {
@@ -31,13 +51,12 @@ public class MessengerActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.signOutMenu) {
-            mainActivity.getAuth().signOut();
-            launchMainActivity();
+            messengerViewModel.signOut();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void launchMainActivity() {
+    private void launchLoginActivity() {
         Intent intent = LoginActivity.newIntent(this);
         startActivity(intent);
     }
